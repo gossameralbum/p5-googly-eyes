37d385a473d9bbb85c0e510b785e765ff9980b2e
gossamer <gossameralbumcover@gmail.com>
2025-03-02 16:58:54 +0300

    added rotate translate and nostroke to the rectangles

--- sketch.js
+++ sketch.js
@@ -17,14 +17,27 @@ function draw() {
 
 	push();
 	translate(leftX, leftY);
-	fill(255);
-	ellipse(0, 0, 50, 50);
 	// Add rectangles to the top of the left eye
+	push();
+	translate(-6.5, -33); // Move 3 pixels upwards
+	rotate(-35);
 	fill(0);
-	rect(-6.5, -35, 3, 10); // Left rectangle
-	rect(3.5, -35, 3, 10); // Right rectangle
+	noStroke();
+	rect(0, -10, 3, 10); // Left rectangle
+	pop();
+	push();
+	translate(3.5, -33); // Move 3 pixels upwards
+	rotate(-35);
+	fill(0);
+	noStroke();
+	rect(0, -10, 3, 10); // Right rectangle
+	pop();
+	fill(255);
+	stroke(255); // Change border color to white
+	ellipse(0, 0, 50, 50);
 	rotate(leftAngle);
 	fill(0);
+	noStroke();
 	ellipse(12.5, 0, 25, 25);
 	pop();
 
@@ -35,14 +48,28 @@ function draw() {
 
 	push();
 	translate(rightX, rightY);
-	fill(255);
-	ellipse(0, 0, 50, 50);
 	// Add rectangles to the top of the right eye
+	push();
+	translate(-0.5, -33); // Move 6 pixels to the right
+	rotate(35); // Lean right 35 degrees
 	fill(0);
-	rect(-6.5, -35, 3, 10); // Left rectangle
-	rect(3.5, -35, 3, 10); // Right rectangle
+	noStroke();
+	rect(0, -10, 3, 10); // Left rectangle
+	pop();
+	push();
+	translate(9.5, -33); // Move 6 pixels to the right
+	rotate(35); // Lean right 35 degrees
+	fill(0);
+	noStroke();
+	rect(0, -10, 3, 10); // Right rectangle
+	pop();
+	// Draw the ellipse
+	fill(255);
+	stroke(255); // Change border color to white
+	ellipse(0, 0, 50, 50);
 	rotate(rightAngle);
 	fill(0);
+	noStroke();
 	ellipse(12.5, 0, 25, 25);
 	pop();
 }
