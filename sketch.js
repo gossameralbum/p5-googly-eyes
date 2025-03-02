1d5e938c631e65309b3a7ac17319e795f6650a9e
gossamer <gossameralbumcover@gmail.com>
2025-03-02 15:32:57 +0300

    finished the project, centered the eyes to the relative degrees of the mage height and width

--- sketch.js
+++ sketch.js
@@ -3,6 +3,38 @@ function setup() {
 	colorMode(RGB, 255, 255, 255);
 }
 
+angleMode(DEGREES);
+
+describe("Two eyes that follow the cursor in front of a purple background.");
+
 function draw() {
 	background(80, 0, 179);
+
+	let leftX = windowWidth / 2 - 100;
+	let leftY = windowHeight / 2;
+
+	let leftAngle = atan2(mouseY - leftY, mouseX - leftX);
+
+	push();
+	translate(leftX, leftY);
+	fill(255);
+	ellipse(0, 0, 50, 50);
+	rotate(leftAngle);
+	fill(0);
+	ellipse(12.5, 0, 25, 25);
+	pop();
+
+	let rightX = windowWidth / 2 + 100;
+	let rightY = windowHeight / 2;
+
+	let rightAngle = atan2(mouseY - rightY, mouseX - rightX);
+
+	push();
+	translate(rightX, rightY);
+	fill(255);
+	ellipse(0, 0, 50, 50);
+	rotate(rightAngle);
+	fill(0);
+	ellipse(12.5, 0, 25, 25);
+	pop();
 }
